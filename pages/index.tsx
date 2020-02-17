3d2e94a68fac285cd90a5ba0553545e14a6a4227
AbrahamLara <abrahamlara19@gmail.com>
2020-02-16 20:10:15 -0500

    chore: Created search bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,55 @@
 import { NextPage } from "next";
 import useSWR from "swr";
+import {
+  InputBase,
+  makeStyles,
+  createStyles,
+  Theme,
+  fade
+} from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
-function fetcher(url) {
-  return fetch(url).then(data => data.json());
-}
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    search: {
+      display: "flex",
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      "&:hover": {
+        backgroundColor: fade(theme.palette.common.white, 0.25)
+      },
+      maxWidth: "400px",
+      margin: "auto"
+    },
+    searchIcon: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "50px"
+    },
+    searchInput: {
+      flex: 1,
+      padding: theme.spacing(1, 1, 1, 0)
+    }
+  })
+);
 
 const Home: NextPage = () => {
-  const { data, error } = useSWR("/api/search?name=batman", fetcher);
+  const classes = useStyles();
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          classes={{ root: classes.searchInput }}
+          placeholder="Superhero..."
+          inputProps={{ "aria-label": "search" }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
