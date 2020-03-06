1df59275237de2f8b199b960f2f2de28bb745339
AbrahamLara <abrahamlara19@gmail.com>
2020-03-06 00:11:17 -0500

    style: Paginator changes size based on screen

--- components/Paginator.tsx
+++ components/Paginator.tsx
@@ -1,17 +1,26 @@
 import Pagination from "@material-ui/lab/Pagination";
 import { connect } from "react-redux";
-import { makeStyles, createStyles } from "@material-ui/core";
+import {
+  makeStyles,
+  createStyles,
+  useMediaQuery,
+  useTheme
+} from "@material-ui/core";
 
 const useStyles = makeStyles(() =>
   createStyles({
     pagination: {
-      margin: "auto",
+      marginLeft: "auto",
+      marginRight: "auto",
+      marginBottom: "20px",
       width: "fit-content"
     }
   })
 );
 
 const Paginator = ({ size, onChange }: any) => {
+  const theme = useTheme();
+  const notSmallScreen = useMediaQuery(theme.breakpoints.up("sm"));
   const classes = useStyles();
   const remainder = size % 10;
   let count = Math.floor(size - remainder) / 10;
@@ -26,7 +35,7 @@ const Paginator = ({ size, onChange }: any) => {
       count={count}
       variant="outlined"
       color="primary"
-      size="large"
+      size={notSmallScreen ? "large" : "medium"}
       onChange={(_: any, page: number) => onChange(page)}
     />
   );
