c58205feedf346d4ce5beb53f1eabc3b9f89d550
AbrahamLara <abrahamlara19@gmail.com>
2020-03-19 00:27:27 -0400

    chore: Setup hero page layout

--- pages/hero/[id].tsx
+++ pages/hero/[id].tsx
@@ -1,12 +1,62 @@
 import { NextPage, NextPageContext } from "next";
-import { useRouter } from "next/router";
 import fetch from "isomorphic-unfetch";
+import {
+  makeStyles,
+  createStyles,
+  Card,
+  CardHeader,
+  Typography,
+  Divider,
+  Box
+} from "@material-ui/core";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    card: {
+      width: "fit-content",
+      margin: "auto"
+    },
+    cardHeaderTitle: {
+      fontSize: "22pt",
+      textAlign: "center"
+    },
+    poster: {
+      display: "block",
+      margin: "auto"
+    },
+    heroInfo: {
+      flex: 1
+    }
+  })
+);
 
 const Hero: NextPage = (props: any) => {
+  const data = props.data;
+  const classes = useStyles();
+
   return (
-    <div>
-      <span>{JSON.stringify(props.data)}</span>
-    </div>
+    <Box display="flex" flexWrap="wrap">
+      <aside style={{ width: "500px" }}>
+        <Card className={classes.card}>
+          <CardHeader
+            title={data.name}
+            classes={{ title: classes.cardHeaderTitle }}
+          />
+          <img
+            className={classes.poster}
+            src={data.image.url}
+            alt={data.name}
+          />
+        </Card>
+      </aside>
+      <div className={classes.heroInfo}>
+        <Typography variant="h4">
+          <p>Powerstats:</p>
+        </Typography>
+        <Divider />
+        <label></label>
+      </div>
+    </Box>
   );
 };
 
